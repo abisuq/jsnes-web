3c5e2c6a04fdb5d92951c083d3bce6e6b0f0fb62
__ <6349395+abisuq@users.noreply.github.com>
2023-04-18 13:18:00 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -32,6 +32,7 @@ class App extends Component {
       <BrowserRouter>
         <div className="App">
           <Route exact path="/" component={ListPage} />
+          <Route exact path="/.com" component={ListPage} />
           <Route exact path="/run" component={RunPage} />
           <Route exact path="/run/:slug" component={RunPage} />
           <Route path="/" render={this.recordPageview} />
